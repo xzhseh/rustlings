34d93a2ce643222e57ba57a09e8fcae484e90fa5
Michael Xu <1378606230@qq.com>
2022-10-05 18:46:08 +0800

    finished macros.rs

---
+++ exercises/clippy/target/debug/deps/clippy1-c976796bcd4b182e.d
@@ -0,0 +1,7 @@
+/home/verta/rust_proj/for_rustlings/rustlings/exercises/clippy/target/debug/deps/clippy1-c976796bcd4b182e.rmeta: clippy1.rs
+
+/home/verta/rust_proj/for_rustlings/rustlings/exercises/clippy/target/debug/deps/clippy1-c976796bcd4b182e.d: clippy1.rs
+
+clippy1.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,12 +1,12 @@
 // macros2.rs
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     my_macro!();
 }
 
+#[macro_export] // Or just move this macro to the above of the main() function
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
