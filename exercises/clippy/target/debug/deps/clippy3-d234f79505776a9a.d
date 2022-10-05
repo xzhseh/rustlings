a11f2f4b8d96cda66f26557b9d11fce032187d41
Michael Xu <1378606230@qq.com>
2022-10-05 18:55:35 +0800

    finished clippy.rs

---
+++ exercises/clippy/target/debug/deps/clippy3-d234f79505776a9a.d
@@ -0,0 +1,7 @@
+/home/verta/rust_proj/for_rustlings/rustlings/exercises/clippy/target/debug/deps/clippy3-d234f79505776a9a.rmeta: clippy3.rs
+
+/home/verta/rust_proj/for_rustlings/rustlings/exercises/clippy/target/debug/deps/clippy3-d234f79505776a9a.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
